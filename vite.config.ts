304f6f22cdb9dd337a666ed231f86bd95bd8b980
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2026-03-18 20:43:55 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
 import { defineConfig } from 'vite'
 import viteSolid from 'vite-plugin-solid'
+import {nitro} from "nitro/vite"
 
 export default defineConfig({
   server: {
     port: 1337,
   },
   plugins: [
-    // nitro(),
+    nitro(),
     tanstackStart(),
     viteSolid({ ssr: true }),
   ],
