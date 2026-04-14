232c1fd501b96a1c1d4b1c0bf5bfc44a900e57c5
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2026-04-14 18:10:15 +0200

    dialog element

---
+++ src/components/Popover/Confirm.tsx
@@ -0,0 +1,68 @@
+import { ComponentProps, JSXElement, mergeProps, Show, splitProps } from "solid-js"
+import styles from "./Confirm.module.css"
+import { StandaloneInput } from "../Forms/FormInput"
+import clickOutside from "~/lib/clickOutside";
+false && clickOutside;
+
+type BaseProps = {
+    headline: string
+    additionalText?: string
+    onConfirm: () => void
+    type?: HTMLInputElement['type']
+    label?: string
+};
+
+type ValueProps =
+    | { challengeAnswer: string; setChallengeAnswer: (val: string) => void; }
+    | { challengeAnswer?: never; setChallengeAnswer?: never; };
+
+type Props = ComponentProps<"dialog"> & {
+    children: JSXElement
+} & BaseProps & ValueProps
+
+export function ConfirmDialog(props: Props) {
+    let dialogElem!: HTMLDialogElement
+    const [local, attributes] = splitProps(props, ["headline", "onConfirm", "type", "label", "challengeAnswer", "setChallengeAnswer", "additionalText"])
+    return (
+        <dialog
+            {...attributes}
+            closedby={props.closedby ?? "any"}
+            ref={dialogElem}
+            class={styles.dialog}
+            use:clickOutside={() => dialogElem?.close()}
+        >
+            <span class={styles.headline} >{props.headline}</span>
+            <Show when={props.additionalText}>
+                <p> {props.additionalText} </p>
+            </Show>
+            <Show when={props.setChallengeAnswer}>
+                <StandaloneInput
+                    field="challengeAnswer"
+                    setter={props.setChallengeAnswer!}
+                    value={props.challengeAnswer!}
+                    label={props.label}
+                    type={props.type}
+                    autofocus
+                />
+            </Show>
+            <div class={styles.btns}>
+                <button 
+                    autofocus
+                    onclick={() => dialogElem.close()}
+                >
+                    Cancel
+                </button>
+            <button
+                disabled={props.setChallengeAnswer && !props.challengeAnswer}
+                onclick={() => {
+                    props.onConfirm()
+                    dialogElem.close()
+                }}
+            >
+                Confirm
+            </button>
+
+            </div>
+        </dialog>
+    )
+}
--- src/components/Popover/MenuPopover.tsx
+++ src/components/Popover/MenuPopover.tsx
@@ -1,9 +1,9 @@
-import { ComponentProps } from "solid-js";
+import { ComponentProps, splitProps } from "solid-js";
 import styles from "./MenuPopover.module.css"
 
-export function MenuPopover(props: {id: string} & ComponentProps<'div'>) {
-
+export function MenuPopover(props: {id: string, popover?: "hint" | "auto" | "manual"} & ComponentProps<'div'>) {
+    const [local, rest] = splitProps(props, ["popover"])
     return (
-        <div {...props} class={`${styles.menu} ${styles.class}`} popover="auto" >{props.children}</div>
+        <div {...rest} class={`${styles.menu} ${styles.class}`} popover={local.popover ?? "auto"} >{props.children}</div>
     )
 }
