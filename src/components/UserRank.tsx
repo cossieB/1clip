b2c9e71e087c7255e89d85dddb04bdc1af0ee0eb
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2026-04-18 17:31:15 +0200

    update deps

--- src/components/UserRank.tsx
+++ src/components/UserRank.tsx
@@ -4,12 +4,12 @@ import { getUserReputation } from "~/serverFn/users";
 import { STORAGE_DOMAIN } from "~/utils/env";
 
 export function UserRank(props: {userId: string, enabled?: boolean}) {
+
     const result = useQuery(() => ({
         queryKey: ["xp", props.userId],
         queryFn: () => getUserReputation({data: props.userId}),
         enabled: props.enabled ?? true
     }))
-
     return (
         <Suspense>
             <img src={`${STORAGE_DOMAIN}ranks/${result.data?.rank}.png`} alt="" />
--- src/features/posts/components/PostAuthor.tsx
+++ src/features/posts/components/PostAuthor.tsx
@@ -16,30 +16,28 @@ export function PostAuthor(props: Props) {
     let timerId: number | undefined
     return (
         <>
-            <div
-                class={props.class}
-                onMouseEnter={(e) => {
-                    window.clearTimeout(timerId)
-                    e.preventDefault()
-                    timerId = window.setTimeout(() => {
-                        document.getElementById("post-author-popover" + props.entityId)?.showPopover()
-                    }, 300)
-                }}
-                onMouseLeave={e => {
-                    window.clearTimeout(timerId)
-                    timerId = window.setTimeout(() => {
-                        document.getElementById("post-author-popover" + props.entityId)?.hidePopover()
-                    }, 300)
-                }}
-            >
+            <div class={props.class} >
                 <div
                     style={{ "anchor-name": "--postAuthor" + props.entityId }}
+                    onMouseEnter={(e) => {
+                        window.clearTimeout(timerId)
+                        e.preventDefault()
+                        timerId = window.setTimeout(() => {
+                            document.getElementById("post-author-popover" + props.entityId)?.showPopover()
+                        }, 300)
+                    }}
+                    onMouseLeave={e => {
+                        window.clearTimeout(timerId)
+                        timerId = window.setTimeout(() => {
+                            document.getElementById("post-author-popover" + props.entityId)?.hidePopover()
+                        }, 300)
+                    }}
                 >
                     <img src={STORAGE_DOMAIN + props.user.image} />
                     <span data-for="user">{props.user.displayUsername}</span>
                     <Link to='/users/$username' params={{ username: props.user.displayUsername! }} />
+                    <UserMiniProfile entityId={props.entityId} userId={props.user.id} />
                 </div>
-                <UserMiniProfile entityId={props.entityId} userId={props.user.id} />
             </div>
         </>
     )
